00af18d3aabbe42fc49d198c616300cc4948e978
Coherent17 <mnb51817@gmail.com>
2024-04-20 10:44:03 -0400

    update img path

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char *argv[]){
     Image *img1 = new GrayImage();
-    img1->LoadImage("/home/course/2024OOP/2024OOPTA/NYCU-OOP-Final-Project/lena.jpg");
+    img1->LoadImage("Image-Folder/lena.jpg");
 
     return 0;
 }
